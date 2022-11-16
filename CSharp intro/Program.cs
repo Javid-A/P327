4fd676c911881cefec71db65834982d85b9fa2c7
Javid-C <javidia@code.edu.az>
2022-11-16 21:48:12 +0400

    C# intro

---
+++ CSharp intro/Program.cs
@@ -0,0 +1,101 @@
+﻿//int num = 5;
+
+//Ekrana hello p237 yazisini cixardan kod
+
+/// <summary>
+///  Returns a number
+/// </summary>
+/// <param name="num"></param>
+/// <param name="num"></param>
+/// <param name="num"></param>
+/// <returns>return 1</returns>
+//Console.WriteLine("Hello P327");
+
+
+
+#region Calculate total
+//int price = 10;
+//int price2 = 20;
+
+//int total = price + price2;
+
+//Console.WriteLine(total); 
+#endregion
+
+//Console.WriteLine("Zehmet olmasa ad daxil edin");
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Daxil olunan ad ==> "+name);
+
+#region Eded cutdur yoxsa tekdir
+
+//Console.WriteLine("Bir eded daxil edin");
+
+//string numStr = Console.ReadLine();
+
+////int num = Convert.ToInt32(numStr);
+
+//int num = int.Parse(numStr);
+
+
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine("Even");
+//}
+//else
+//{
+//    Console.WriteLine("Odd");
+//} 
+#endregion
+
+
+#region Eded 3-e bolunur mu
+//Console.WriteLine("Zehmet olmasa eded daxil edin");
+
+////string numStr = Console.ReadLine();
+//int num = int.Parse(Console.ReadLine());
+
+//if(num %3 == 0)
+//{
+//    if(num%2 == 0)
+//    {
+//        Console.WriteLine("Bolunur ve cutdur");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Bolunur ve tekdir");
+//    }
+
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//} 
+#endregion
+
+
+#region Factorial
+//Console.WriteLine("Please enter valid number");
+
+//string numStr = Console.ReadLine();
+
+//int num = Convert.ToInt32(numStr);
+
+//int result = 1;
+//for (int i = 1; i <= num; i++)
+//{
+//    result *= i;
+//}
+//Console.WriteLine("Result: "+result); 
+#endregion
+
+//Console.WriteLine("Hello");
+//Console.WriteLine("World");
+
+//Console.Write("Hello ");
+//Console.Write("P327");
+//Console.WriteLine("Asiman");
+//Console.Write("Test");
+
+
+//Console.WriteLine("Bu bir neticedir:\t20");
